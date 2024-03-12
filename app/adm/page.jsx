f64f7b40afbf78e35ccd2b048e38323d12900f89
unknown <ezekuiel100@gmail.com>
2024-03-12 15:36:54 -0300

    Middleware

--- app/adm/page.jsx
+++ app/adm/page.jsx
@@ -4,16 +4,15 @@ import { redirect } from "next/navigation";
 
 async function Adm() {
   const session = await getServerSession();
-  console.log(session);
 
-  if (!session) {
-    redirect("/");
+  if (session) {
+    redirect("/adm");
   }
 
   return (
     <div className="h-screen flex justify-center items-center ">
       <div className="">
-        <p className="text-2xl">Bem vindo, {session.user.name} </p>
+        <p className="text-2xl">Bem vindo, {session?.user.name} </p>
         <Logout />
       </div>
     </div>
